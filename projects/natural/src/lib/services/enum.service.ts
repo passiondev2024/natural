110ce4bc06186a853dcc17d2ac52ffca2c2bb6a4
kanandev2024 <kananramazandev@gmail.com>
2019-04-20 00:34:36 +0200

    re-activate enum service

--- projects/natural/src/lib/services/enum.service.ts
+++ projects/natural/src/lib/services/enum.service.ts
@@ -1,14 +1,26 @@
 import { Injectable } from '@angular/core';
 import { Apollo } from 'apollo-angular';
-import { Observable, of } from 'rxjs';
+import gql from 'graphql-tag';
+import { Observable } from 'rxjs';
 import { map } from 'rxjs/operators';
-// import { enumTypeQuery } from '../../shared/queries/enum';
+
+export const enumTypeQuery = gql`
+    query EnumType($name: String!) {
+        __type(name:$name) {
+            __typename
+            enumValues {
+                name
+                description
+            }
+        }
+    }`;
 
 export interface IEnum {
     value: string;
     name: string;
 }
 
+// GENERIC should be enumTypeQuery from generated types
 @Injectable({
     providedIn: 'root',
 })
@@ -22,25 +34,23 @@ export class NaturalEnumService<EnumType> {
      */
     public get(name: string): Observable<IEnum[]> {
 
-        return of([]);
-
         // Load possible action statuses
-        // return this.apollo.query<EnumType>({
-        //     query: enumTypeQuery, // TODO : this is missing ! find a way to provide
-        //     variables: {name: name},
-        //     fetchPolicy: 'cache-first',
-        // }).pipe(map(result => {
-        //     const values: IEnum[] = [];
-        //     if (result.data.__type && result.data.__type.enumValues) {
-        //         for (const enumValue of result.data.__type.enumValues) {
-        //             values.push({
-        //                 value: enumValue.name,
-        //                 name: enumValue.description || '',
-        //             });
-        //         }
-        //     }
-        //     return values;
-        // }));
+        return this.apollo.query<EnumType>({
+            query: enumTypeQuery,
+            variables: {name: name},
+            fetchPolicy: 'cache-first',
+        }).pipe(map((result: any) => {
+            const values: IEnum[] = [];
+            if (result.data.__type && result.data.__type.enumValues) {
+                for (const enumValue of result.data.__type.enumValues) {
+                    values.push({
+                        value: enumValue.name,
+                        name: enumValue.description || '',
+                    });
+                }
+            }
+            return values;
+        }));
     }
 
 }
