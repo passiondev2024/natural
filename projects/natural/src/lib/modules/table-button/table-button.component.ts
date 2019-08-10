ccdab737144ddb1b824608eb81f981bd117543bb
kanandev2024 <kananramazandev@gmail.com>
2019-08-10 13:22:00 -0700

    natural-table-button: open external link in new tab

--- projects/natural/src/lib/modules/table-button/table-button.component.ts
+++ projects/natural/src/lib/modules/table-button/table-button.component.ts
@@ -1,6 +1,12 @@
 import { Component, Input, ViewEncapsulation } from '@angular/core';
-import { RouterLink } from '@angular/router';
+import { QueryParamsHandling, RouterLink } from '@angular/router';
 
+/**
+ * Button that fits well in a `<mat-table>` and support either
+ * route navigation via `navigate` or external URL via `href`.
+ *
+ * External URL will always be opened in new tab.
+ */
 @Component({
     selector: 'natural-table-button',
     templateUrl: './table-button.component.html',
@@ -9,8 +15,10 @@ import { RouterLink } from '@angular/router';
 })
 export class NaturalTableButtonComponent {
 
-    @Input() queryParams: any;
-    @Input() queryParamsHandling: any;
+    @Input() queryParams: {
+        [k: string]: any;
+    };
+    @Input() queryParamsHandling: QueryParamsHandling;
     @Input() label: string;
     @Input() icon: string;
     @Input() href: string;
