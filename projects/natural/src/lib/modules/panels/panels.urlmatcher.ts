fbcd5cd417afe4c20f6b36db555b553c8e16777b
kanandev2024 <kananramazandev@gmail.com>
2019-08-12 21:56:20 +0200

    NaturalUrlMatcher always return array to match functions signature

--- projects/natural/src/lib/modules/panels/panels.urlmatcher.ts
+++ projects/natural/src/lib/modules/panels/panels.urlmatcher.ts
@@ -1,11 +1,6 @@
 import { UrlMatchResult, UrlSegment } from '@angular/router';
 import { NaturalPanelsService } from './panels.service';
 
-export function NaturalPanelsUrlMatcher(segments: UrlSegment[]): UrlMatchResult | null {
-    const result = {consumed: NaturalPanelsService.getConsumedSegments(segments)};
-    if (result.consumed.length) {
-        return result;
-    } else {
-        return null;
-    }
+export function NaturalPanelsUrlMatcher(segments: UrlSegment[]): UrlMatchResult {
+    return {consumed: NaturalPanelsService.getConsumedSegments(segments)};
 }
