b7739a74dbd69cb6243491d416ef15b17de324d0
kanandev2024 <kananramazandev@gmail.com>
2019-08-26 14:39:32 -0700

    Typed deepClone

--- projects/natural/src/lib/modules/search/classes/utils.ts
+++ projects/natural/src/lib/modules/search/classes/utils.ts
@@ -16,6 +16,6 @@ export function getFacetFromSelection(facets: NaturalSearchFacets | null,
            null;
 }
 
-export function deepClone(obj: any): any {
+export function deepClone<T>(obj: T): T {
     return JSON.parse(JSON.stringify(obj));
 }
