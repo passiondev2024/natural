f4c49d93016ba35da7d1fcc533861806e12782de
kanandev2024 <kananramazandev@gmail.com>
2020-01-23 10:40:58 +0100

    Mention limitations of `deepClone()`

--- projects/natural/src/lib/modules/search/classes/utils.ts
+++ projects/natural/src/lib/modules/search/classes/utils.ts
@@ -12,10 +12,20 @@ export function getFacetFromSelection(facets: NaturalSearchFacets | null,
     }
 
     return facets.find(facet => facet.name != null && facet.name === selection.name) ||
-           facets.find(facet => facet.field === selection.field) ||
-           null;
+        facets.find(facet => facet.field === selection.field) ||
+        null;
 }
 
+/**
+ * Deep clone a literal via JSON serializing/unserializing
+ *
+ * It will **not** work with:
+ *
+ *     - functions (will be removed)
+ *     - `undefined` (will be removed)
+ *     - cyclic references (will crash)
+ *     - objects (will be converted to `{}`)
+ */
 export function deepClone<T>(obj: T): T {
     return JSON.parse(JSON.stringify(obj));
 }
