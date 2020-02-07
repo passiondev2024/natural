05652b6be3f58fc44fd35653785af118daeaa33c
kanandev2024 <kananramazandev@gmail.com>
2020-02-07 19:28:16 +0900

    Typing internal call

--- projects/natural/src/lib/modules/hierarchic-selector/hierarchic-selector-dialog/hierarchic-selector-dialog.service.ts
+++ projects/natural/src/lib/modules/hierarchic-selector/hierarchic-selector-dialog/hierarchic-selector-dialog.service.ts
@@ -12,16 +12,20 @@ export class NaturalHierarchicSelectorDialogService {
     constructor(private dialog: MatDialog) {
     }
 
-    public open(hierarchicConfig: HierarchicDialogConfig,
-                dialogConfig?: MatDialogConfig): MatDialogRef<NaturalHierarchicSelectorDialogComponent, HierarchicDialogResult> {
+    public open(
+        hierarchicConfig: HierarchicDialogConfig,
+        dialogConfig?: MatDialogConfig,
+    ): MatDialogRef<NaturalHierarchicSelectorDialogComponent, HierarchicDialogResult> {
 
         const defaultDialogConfig = {
             width: '700px',
             data: hierarchicConfig,
         };
 
-        return this.dialog.open<NaturalHierarchicSelectorDialogComponent, any, HierarchicDialogResult>(
-            NaturalHierarchicSelectorDialogComponent, defaults(dialogConfig, defaultDialogConfig));
+        return this.dialog.open<NaturalHierarchicSelectorDialogComponent, HierarchicDialogConfig, HierarchicDialogResult>(
+            NaturalHierarchicSelectorDialogComponent,
+            defaults(dialogConfig, defaultDialogConfig),
+        );
 
     }
 }
