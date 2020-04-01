215cc58b9cc3d70482a157ec686f1fef6edb1cef
kanandev2024 <kananramazandev@gmail.com>
2020-04-01 12:12:08 +0900

    Delay errors instead of throwing them right away

--- projects/natural/src/lib/testing/error.service.ts
+++ projects/natural/src/lib/testing/error.service.ts
@@ -1,13 +1,19 @@
 import { Injectable } from '@angular/core';
-import { Validators } from '@angular/forms';
-import { Router } from '@angular/router';
 import { Apollo } from 'apollo-angular';
-import { Observable, of, throwError } from 'rxjs';
+import { Observable, timer } from 'rxjs';
 import { PaginatedData } from '../classes/data-source';
 import { NaturalQueryVariablesManager, QueryVariables } from '../classes/query-variable-manager';
-import { FormValidators, NaturalAbstractModelService } from '../services/abstract-model.service';
-import { delay } from 'rxjs/operators';
+import { NaturalAbstractModelService } from '../services/abstract-model.service';
+import { map } from 'rxjs/operators';
 import { Item } from './any.service';
+
+function error(method: string) {
+    return timer(1000).pipe(
+        map(() => {
+            throw new Error('ErrorService.' + method + ' error');
+        }),
+    );
+}
 
 @Injectable({
     providedIn: 'root',
@@ -22,53 +28,36 @@ export class ErrorService extends NaturalAbstractModelService<Item,
     never,
     never> {
 
-    constructor(
-        apollo: Apollo,
-        protected router: Router,
-    ) {
-        super(apollo,
+    constructor(apollo: Apollo) {
+        super(
+            apollo,
             'user',
             null,
             null,
             null,
             null,
-            null);
+            null,
+        );
     }
 
     public watchAll(
         queryVariablesManager: NaturalQueryVariablesManager<QueryVariables>,
         expire: Observable<void>,
     ): Observable<PaginatedData<Item>> {
-        return throwError(new Error('ErrorService.watchAll error')).pipe(delay(1000));
+        return error('watchAll');
     }
 
     public getAll(
         queryVariablesManager: NaturalQueryVariablesManager<QueryVariables>,
     ): Observable<PaginatedData<Item>> {
-        return throwError(new Error('ErrorService.getAll error')).pipe(delay(1000));
+        return error('getAll');
     }
 
     public getOne(id: string): Observable<Item> {
-        return throwError(new Error('ErrorService.getOne error')).pipe(delay(1000));
-    }
-
-    protected getDefaultForClient() {
-        return {
-            name: '',
-            description: '',
-            children: [],
-            parent: null,
-        };
-    }
-
-    public getFormValidators(): FormValidators {
-        return {
-            name: [Validators.required, Validators.maxLength(10)],
-            description: [Validators.maxLength(20)],
-        };
+        return error('getOne');
     }
 
     public count(queryVariablesManager: unknown): Observable<number> {
-        return throwError(new Error('ErrorService.count error')).pipe(delay(1000));
+        return error('count');
     }
 }
