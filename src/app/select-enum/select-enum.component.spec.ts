5f13a25c40c70d3a96d88d2f38dbea567633ccf0
kanandev2024 <kananramazandev@gmail.com>
2020-05-23 12:54:07 +0900

    Fix demo tests

--- src/app/select-enum/select-enum.component.spec.ts
+++ src/app/select-enum/select-enum.component.spec.ts
@@ -1,5 +1,5 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
-import { FormsModule } from '@angular/forms';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { NoopAnimationsModule } from '@angular/platform-browser/animations';
 import { RouterTestingModule } from '@angular/router/testing';
 import {
@@ -25,6 +25,7 @@ describe('Demo SelectComponent', () => {
                 NoopAnimationsModule,
                 MaterialModule,
                 FormsModule,
+                ReactiveFormsModule,
                 NaturalSelectEnumModule,
                 ApolloTestingModule,
                 NaturalHierarchicSelectorModule,
