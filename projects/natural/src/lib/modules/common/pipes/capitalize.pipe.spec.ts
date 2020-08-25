696aacbd6202836d7c706547da2dbe815ed04946
kanandev2024 <kananramazandev@gmail.com>
2020-08-25 18:11:46 +0900

    Drop support of Observable in NaturalCapitalizePipe
    Instead the pipe `async` should be used before this pipe.
    
    Keeping support would have mean that all pipes universally should
    support observables, which is not reasonnable.

--- projects/natural/src/lib/modules/common/pipes/capitalize.pipe.spec.ts
+++ projects/natural/src/lib/modules/common/pipes/capitalize.pipe.spec.ts
@@ -1,4 +1,3 @@
-import {BehaviorSubject, Observable} from 'rxjs';
 import {NaturalCapitalizePipe} from './capitalize.pipe';
 
 describe('NaturalCapitalizePipe', () => {
@@ -20,21 +19,5 @@ describe('NaturalCapitalizePipe', () => {
     it('do not crash with null', () => {
         const pipe = new NaturalCapitalizePipe();
         expect(pipe.transform(null)).toBeNull();
-    });
-
-    it('capitalize a string within an observable', () => {
-        const pipe = new NaturalCapitalizePipe();
-        const obs1 = new BehaviorSubject('foo bar');
-        let actual1 = 'initial';
-        (pipe.transform(obs1) as Observable<string>).subscribe(v => (actual1 = v));
-        expect(actual1).toBe('Foo bar');
-    });
-
-    it('do not crash with null within an observable', () => {
-        const pipe = new NaturalCapitalizePipe();
-        const obs1 = new BehaviorSubject(null);
-        let actual1 = 'initial';
-        (pipe.transform(obs1) as Observable<string>).subscribe(v => (actual1 = v));
-        expect(actual1).toBeNull();
     });
 });
--- projects/natural/src/lib/modules/common/pipes/capitalize.pipe.ts
+++ projects/natural/src/lib/modules/common/pipes/capitalize.pipe.ts
@@ -1,22 +1,13 @@
 import {Pipe, PipeTransform} from '@angular/core';
-import {Observable} from 'rxjs';
-import {map} from 'rxjs/operators';
 import {upperCaseFirstLetter} from '../../../classes/utility';
 
 @Pipe({name: 'capitalize'})
 export class NaturalCapitalizePipe implements PipeTransform {
-    public transform(value: string | null | Observable<string | null>): string | null | Observable<string | null> {
-        if (value instanceof Observable) {
-            return value.pipe(map(val => this.getValue(val)));
-        } else {
-            return this.getValue(value);
-        }
-    }
-
-    private getValue(value: string | null): string | null {
+    public transform(value: string | null): string | null {
         if (value) {
             return upperCaseFirstLetter(value);
         }
+
         return value;
     }
 }
