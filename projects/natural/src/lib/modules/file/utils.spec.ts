88472c8ab53c702974eb01c13f42e7a24b5d3f6d
kanandev2024 <kananramazandev@gmail.com>
2020-09-25 09:39:47 +0900

    Restore tests for acceptType

--- projects/natural/src/lib/modules/file/utils.spec.ts
+++ projects/natural/src/lib/modules/file/utils.spec.ts
@@ -1,29 +1,36 @@
 import {acceptType} from './utils';
 
 describe('acceptType', () => {
-    it('.png,.jpeg,.gif', () => {
-        const accept = '.png,.jpeg,.gif';
-        expect(acceptType(accept, 'text/comma-separated-values')).toBe(false);
-        expect(acceptType(accept, 'image/jpeg')).toBe(true);
-        expect(acceptType(accept, 'image/gif')).toBe(true);
-        expect(acceptType(accept, 'image/png')).toBe(true);
+    it('accept anything', () => {
+        expect(acceptType('', 'image/jpeg', 'foo.jpg')).toBe(true);
+        expect(acceptType(' ', 'image/jpeg', 'foo.jpg')).toBe(true);
     });
 
-    it('.mxf,video/*', () => {
-        const accept = '.mxf,video/*';
-        expect(acceptType(accept, 'mxf/video')).toBe(false);
-        expect(acceptType(accept, 'audio/mxf')).toBe(true);
-        expect(acceptType(accept, 'video/mxf')).toBe(true);
-        expect(acceptType(accept, '', '<my-file>.mxf')).toBe(true);
+    it('.png,.jpg,.gif', () => {
+        const accept = '.png,.jpg,.gif';
+        expect(acceptType(accept, 'text/comma-separated-values', 'foo.csv')).toBe(false);
+        expect(acceptType(accept, 'image/jpeg', 'foo.jpg')).toBe(true);
+        expect(acceptType(accept, 'image/gif', 'foo.gif')).toBe(true);
+        expect(acceptType(accept, 'image/png', 'foo.png')).toBe(true);
+    });
+
+    it('.mp4,video/*', () => {
+        const accept = '.mp4,video/*';
+        expect(acceptType(accept, 'mp4/video', 'foo.avi')).toBe(false);
+        expect(acceptType(accept, 'audio/mp4', 'foo.avi')).toBe(false);
+        expect(acceptType(accept, 'video/mp4', 'foo.avi')).toBe(true);
+        expect(acceptType(accept, 'audio/mp4', 'foo.mp4')).toBe(true);
+        expect(acceptType(accept, 'video/mp4', 'foo.mp4')).toBe(true);
+        expect(acceptType(accept, '', 'foo.mp4')).toBe(true);
     });
 
     it('extension test', () => {
         const accept = '.pdf, .jpg, .tif, .gif, .bmp, .jpeg';
-        expect(acceptType(accept, 'image/gif')).toBe(true);
-        expect(acceptType(accept, 'file.pdf')).toBe(true);
-        expect(acceptType(accept, 'file.file.bmp')).toBe(true);
-        expect(acceptType(accept, 'file.file.gif.xyx')).toBe(false);
-        expect(acceptType(accept, 'video/mxf')).toBe(false);
-        expect(acceptType(accept, '', '<my-file>.mxf')).toBe(false);
+        expect(acceptType(accept, 'image/gif', 'foo.gif')).toBe(true);
+        expect(acceptType(accept, 'application/pdf', 'foo.pdf')).toBe(true);
+        expect(acceptType(accept, 'image/bmp', 'foo.bmp')).toBe(true);
+        expect(acceptType(accept, '', 'foo.gif.xyx')).toBe(false);
+        expect(acceptType(accept, 'video/mp4', 'foo.mp4')).toBe(false);
+        expect(acceptType(accept, '', 'foo.mp4')).toBe(false);
     });
 });
--- projects/natural/src/lib/modules/file/utils.ts
+++ projects/natural/src/lib/modules/file/utils.ts
@@ -1,38 +1,22 @@
-export function acceptType(accept: string, type: string, name?: string): boolean {
-    if (!accept) {
+export function acceptType(accept: string, type: string, filename: string): boolean {
+    if (!accept.trim()) {
         return true;
     }
 
-    const defs = accept.split(',');
-    let regx: RegExp;
-    let acceptRegString: string;
+    type = type.toLowerCase();
+    filename = filename.toLowerCase();
 
-    for (let x = defs.length - 1; x >= 0; --x) {
-        // Escapes dots in mimetype
-        acceptRegString = defs[x];
-        // trim
-        acceptRegString = acceptRegString.replace(/(^\s+|\s+$)/g, '');
-        // Escapes stars in mimetype
-        acceptRegString = acceptRegString.replace(/\*/g, '.*');
-        // let acceptReg = '^((' + acceptRegString
-        // acceptReg = acceptReg.replace(/,/g,')|(') + '))$'
+    return accept.split(',').some(mimeOrExtension => {
+        mimeOrExtension = mimeOrExtension.trim().toLowerCase();
+        if (mimeOrExtension.startsWith('.')) {
+            return filename.endsWith(mimeOrExtension);
+        } else {
+            // Transform `*` into `.*`
+            const pattern = mimeOrExtension.replace(/\*/g, '.*');
 
-        // try by mime
-        regx = new RegExp(acceptRegString, 'gi');
-        if (type.search(regx) >= 0) {
-            return true;
+            return type.match(pattern);
         }
-
-        // try by ext
-        if (acceptRegString.substring(0, 1) === '.') {
-            acceptRegString = '\\' + acceptRegString; // .substring(1, acceptRegString.length-1)//remove dot at front
-            regx = new RegExp(acceptRegString + '$', 'i');
-            if ((name || type).search(regx) >= 0) {
-                return true;
-            }
-        }
-    }
-    return false;
+    });
 }
 
 export function isFileInput(elm: any): boolean {
