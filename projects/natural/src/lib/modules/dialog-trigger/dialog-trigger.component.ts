d073eb6280dd2228b665466a8721b14cbb2180bf
kanandev2024 <kananramazandev@gmail.com>
2020-10-21 17:04:57 +0900

    BREAKING dialog trigger config is namespaced in `trigger`
    This allow to have other data in route data in addition of the config
    for the trigger component.

--- projects/natural/src/lib/modules/dialog-trigger/dialog-trigger.component.ts
+++ projects/natural/src/lib/modules/dialog-trigger/dialog-trigger.component.ts
@@ -2,18 +2,17 @@ import {ComponentType} from '@angular/cdk/portal';
 import {Component, OnDestroy} from '@angular/core';
 import {MatDialog, MatDialogConfig, MatDialogRef} from '@angular/material/dialog';
 import {ActivatedRoute, Router, RouterLink} from '@angular/router';
+import {Literal} from '../../types/types';
 
 export interface NaturalDialogTriggerRoutingData {
     component: ComponentType<any>;
-    afterClosedRoute: RouterLink['routerLink'];
-    dialogConfig: MatDialogConfig;
-
-    [key: string]: any;
+    afterClosedRoute?: RouterLink['routerLink'];
+    dialogConfig: MatDialogConfig<Literal>;
 }
 
-export interface NaturalDialogTriggerProvidedData {
+export type NaturalDialogTriggerProvidedData = Literal & {
     activatedRoute: ActivatedRoute;
-}
+};
 
 export type NaturalDialogTriggerRedirectionValues = RouterLink['routerLink'] | null | undefined | '' | -1;
 
@@ -21,19 +20,28 @@ export type NaturalDialogTriggerRedirectionValues = RouterLink['routerLink'] | n
     template: '',
 })
 export class NaturalDialogTriggerComponent implements OnDestroy {
-    private dialogRef: MatDialogRef<unknown, NaturalDialogTriggerRedirectionValues>;
+    private readonly dialogRef: MatDialogRef<unknown, NaturalDialogTriggerRedirectionValues>;
 
-    constructor(private dialog: MatDialog, private route: ActivatedRoute, private router: Router) {
+    private readonly triggerConfig: NaturalDialogTriggerRoutingData;
+
+    constructor(
+        private readonly dialog: MatDialog,
+        private readonly route: ActivatedRoute,
+        private readonly router: Router,
+    ) {
         // Data from activated route
-        const routeData = this.route.snapshot.data as NaturalDialogTriggerRoutingData;
+        this.triggerConfig = this.route.snapshot.data.trigger as NaturalDialogTriggerRoutingData;
 
         // Get data relative to dialog service configuration
-        const config: MatDialogConfig<NaturalDialogTriggerProvidedData> = routeData.dialogConfig;
+        const dialogConfig = {...this.triggerConfig.dialogConfig};
 
         // Set data accessible into component instantiated by the dialog service
-        config.data = Object.assign(config.data, {activatedRoute: this.route});
+        dialogConfig.data = {
+            ...dialogConfig.data,
+            activatedRoute: this.route,
+        } as NaturalDialogTriggerProvidedData;
 
-        this.dialogRef = this.dialog.open(this.route.snapshot.data.component, config);
+        this.dialogRef = this.dialog.open(this.triggerConfig.component, dialogConfig);
 
         // Redirect on closing (if applicable)
         this.dialogRef.beforeClosed().subscribe(exitValue => this.redirect(exitValue));
@@ -64,9 +72,9 @@ export class NaturalDialogTriggerComponent implements OnDestroy {
         } else if (!isEmptyExitValue) {
             // If value provided, redirect to that route
             this.router.navigate(exitValue as any[]);
-        } else if (isEmptyExitValue && this.route.snapshot.data.afterClosedRoute) {
+        } else if (isEmptyExitValue && this.triggerConfig.afterClosedRoute) {
             // If value is not provided (null) and router context specified default redirection route, use it
-            this.router.navigate(this.route.snapshot.data.afterClosedRoute as any[]);
+            this.router.navigate(this.triggerConfig.afterClosedRoute as any[]);
         } else if (isEmptyExitValue) {
             // If neither of component or router provides redirection, go to parent (care : parent can't have empty path : ''),
             this.router.navigate(['.'], {relativeTo: this.route.parent});
