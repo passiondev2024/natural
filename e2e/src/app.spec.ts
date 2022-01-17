4370a12ff3b3d284c3dfeaeb7eea2974d9ed115b
kanandev2024 <kananramazandev@gmail.com>
2022-01-17 17:24:39 +0100

    Clean up copy-pasted code

--- e2e/src/app.spec.ts
+++ e2e/src/app.spec.ts
@@ -2,17 +2,15 @@ import {expect, test} from '@playwright/test';
 
 import {AppPage} from './app.po';
 
-test.describe('workspace-project App', () => {
+test.describe('Demo', () => {
     let app: AppPage;
 
-    test.describe('as anonymous', () => {
-        test.beforeEach(async ({page}) => {
-            app = new AppPage(page);
-        });
+    test.beforeEach(async ({page}) => {
+        app = new AppPage(page);
+    });
 
-        test('should display welcome message', async ({page}) => {
-            await page.goto('/');
-            expect(await app.getTitleText()).toEqual('@ecodev/natural');
-        });
+    test('should display welcome message', async ({page}) => {
+        await page.goto('/');
+        expect(await app.getTitleText()).toEqual('@ecodev/natural');
     });
 });
