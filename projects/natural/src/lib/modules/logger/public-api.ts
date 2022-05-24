ef613459c65c9b65838f2d9b0415d0afaace33e4
kanandev2024 <kananramazandev@gmail.com>
2022-05-24 15:59:14 +0200

    Be specific with public API
    To avoid accidentally publishing that we did not mean to

--- projects/natural/src/lib/modules/logger/public-api.ts
+++ projects/natural/src/lib/modules/logger/public-api.ts
@@ -2,5 +2,12 @@
  * Public API Surface of natural
  */
 
-export * from './error.module';
-export * from './error-handler';
+export {NaturalErrorModule} from './error.module';
+
+export {
+    NaturalErrorHandler,
+    NaturalLoggerConfigExtra,
+    NaturalLoggerConfigUrl,
+    NaturalLoggerExtra,
+    NaturalLoggerType,
+} from './error-handler';
