e931b7aa5d871ac5cbc471b32eff30f87b770004
kanandev2024 <kananramazandev@gmail.com>
2022-05-24 14:20:22 +0200

    `debug` also show subscribes

--- projects/natural/src/lib/classes/rxjs.ts
+++ projects/natural/src/lib/classes/rxjs.ts
@@ -45,6 +45,7 @@ export function cancellableTimeout(canceller: Observable<unknown>, milliSeconds:
  */
 export function debug<T>(debugName: string): MonoTypeOperatorFunction<T> {
     return tap<T>({
+        subscribe: () => console.log('SUBSCRIBE', debugName),
         next: value => console.log('NEXT', debugName, value),
         error: error => console.log('ERROR', debugName, error),
         complete: () => console.log('COMPLETE', debugName),
