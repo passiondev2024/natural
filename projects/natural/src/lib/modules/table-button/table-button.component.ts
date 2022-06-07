0a4d2918f1f65562553a49faf9926eff7324c219
kanandev2024 <kananramazandev@gmail.com>
2022-06-07 14:50:31 +0200

    natural-table-button fallbacks to span when no link is given #8905
    In some edge case we don't have any links to link to. Eg: because the
    viewer is not allowed to access the listed item. In this case we allow
    to gracefully fallback on a simple span to still show the icon and/or
    label, but not as button that would not do anything.

--- projects/natural/src/lib/modules/table-button/table-button.component.ts
+++ projects/natural/src/lib/modules/table-button/table-button.component.ts
@@ -1,10 +1,13 @@
-import {Component, Input, ViewEncapsulation} from '@angular/core';
-import {QueryParamsHandling, RouterLink} from '@angular/router';
+import {Component, Input, OnChanges, SimpleChanges, ViewEncapsulation} from '@angular/core';
+import {Params, QueryParamsHandling, RouterLink} from '@angular/router';
 import {ThemePalette} from '@angular/material/core';
 
 /**
  * Button that fits well in a `<mat-table>` and support either
  * route navigation via `navigate` or external URL via `href`.
+ *
+ * If neither `navigate` nor `href` has a meaningful value, then
+ * it will show the icon and/or label in `<span>` instead of a button
  *
  * External URL will always be opened in new tab.
  */
@@ -14,10 +17,8 @@ import {ThemePalette} from '@angular/material/core';
     styleUrls: ['./table-button.component.scss'],
     encapsulation: ViewEncapsulation.None,
 })
-export class NaturalTableButtonComponent {
-    @Input() public queryParams: {
-        [k: string]: any;
-    } = {};
+export class NaturalTableButtonComponent implements OnChanges {
+    @Input() public queryParams: Params = {};
     @Input() public queryParamsHandling: QueryParamsHandling = '';
     @Input() public label?: string | null;
     @Input() public icon?: string | null;
@@ -27,6 +28,15 @@ export class NaturalTableButtonComponent {
     @Input() public preserveFragment = false;
     @Input() public raised = false;
     @Input() public color: ThemePalette;
+    public type: 'routerLink' | 'href' | 'none' = 'none';
 
-    public constructor() {}
+    public ngOnChanges(changes: SimpleChanges): void {
+        if (this.navigate?.length || Object.keys(this.queryParams).length) {
+            this.type = 'routerLink';
+        } else if (this.href) {
+            this.type = 'href';
+        } else {
+            this.type = 'none';
+        }
+    }
 }
--- src/app/other/other.component.ts
+++ src/app/other/other.component.ts
@@ -1,6 +1,21 @@
 import {HttpClient} from '@angular/common/http';
 import {Component, OnInit} from '@angular/core';
 import {FormControl, FormGroup, Validators} from '@angular/forms';
+import {Params, QueryParamsHandling, RouterLink} from '@angular/router';
+import {ThemePalette} from '@angular/material/core';
+
+type TableButtonConfiguration = {
+    label?: string | null;
+    icon?: string | null;
+    href?: string | null;
+    navigate: RouterLink['routerLink'];
+    queryParams: Params;
+    queryParamsHandling: QueryParamsHandling;
+    fragment?: string | undefined;
+    preserveFragment: boolean;
+    raised: boolean;
+    color: ThemePalette;
+};
 
 @Component({
     selector: 'app-other',
@@ -12,6 +27,104 @@ export class OtherComponent implements OnInit {
      * Single control
      */
     public httpPrefixControl = new FormControl('', [Validators.required]);
+    public configurations: TableButtonConfiguration[] = [
+        {
+            label: 'my label without any link',
+            icon: undefined,
+            href: undefined,
+            navigate: [],
+            queryParams: {},
+            queryParamsHandling: '',
+            fragment: undefined,
+            preserveFragment: false,
+            raised: false,
+            color: undefined,
+        },
+        {
+            label: undefined,
+            icon: 'home',
+            href: undefined,
+            navigate: [],
+            queryParams: {},
+            queryParamsHandling: '',
+            fragment: undefined,
+            preserveFragment: false,
+            raised: false,
+            color: undefined,
+        },
+        {
+            label: 'my label without any link',
+            icon: 'home',
+            href: undefined,
+            navigate: [],
+            queryParams: {},
+            queryParamsHandling: '',
+            fragment: undefined,
+            preserveFragment: false,
+            raised: false,
+            color: undefined,
+        },
+        {
+            label: 'my label with undefined navigate',
+            icon: 'home',
+            href: undefined,
+            navigate: undefined,
+            queryParams: {},
+            queryParamsHandling: '',
+            fragment: undefined,
+            preserveFragment: false,
+            raised: false,
+            color: undefined,
+        },
+        {
+            label: 'my label with routerLink',
+            icon: undefined,
+            href: undefined,
+            navigate: ['/search'],
+            queryParams: {},
+            queryParamsHandling: '',
+            fragment: undefined,
+            preserveFragment: false,
+            raised: false,
+            color: undefined,
+        },
+        {
+            label: 'my label with href',
+            icon: undefined,
+            href: '/search',
+            navigate: [],
+            queryParams: {},
+            queryParamsHandling: '',
+            fragment: undefined,
+            preserveFragment: false,
+            raised: false,
+            color: undefined,
+        },
+        {
+            label: 'my label with queryParams but no routerLink',
+            icon: undefined,
+            href: undefined,
+            navigate: [],
+            queryParams: {foo: 'bar'},
+            queryParamsHandling: '',
+            fragment: undefined,
+            preserveFragment: false,
+            raised: false,
+            color: undefined,
+        },
+        {
+            label: 'my label with queryParams and routerLink',
+            icon: 'home',
+            href: undefined,
+            navigate: ['/search'],
+            queryParams: {foo: 'bar'},
+            queryParamsHandling: '',
+            fragment: undefined,
+            preserveFragment: false,
+            raised: false,
+            color: undefined,
+        },
+    ];
 
     /**
      * Form group
