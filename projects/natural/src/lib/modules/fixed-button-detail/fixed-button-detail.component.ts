20b6e2bbbfab4ec48caff4e6ec4162a272f5f2ae
kanandev2024 <kananramazandev@gmail.com>
2022-08-31 17:04:28 +0200

    Don't show delete button on create page #8589
    We used to show the delete button for a very short time between end of
    creation and redirection to next page.
    
    Now the button has a creation mode which is changed only when the page
    changes. So even if the model receive an ID after creation, the button
    will still stay in creation mode. The only way to change mode is to
    navigate to another page.

--- projects/natural/src/lib/modules/fixed-button-detail/fixed-button-detail.component.ts
+++ projects/natural/src/lib/modules/fixed-button-detail/fixed-button-detail.component.ts
@@ -1,5 +1,13 @@
 import {Component, EventEmitter, Input, Output} from '@angular/core';
 import {FormGroup} from '@angular/forms';
+import {ActivatedRoute} from '@angular/router';
+
+type Model = {
+    id?: string;
+    permissions?: {
+        delete: boolean;
+    };
+};
 
 @Component({
     selector: 'natural-fixed-button-detail',
@@ -7,17 +15,31 @@ import {FormGroup} from '@angular/forms';
     styleUrls: ['./fixed-button-detail.component.scss'],
 })
 export class NaturalFixedButtonDetailComponent {
-    @Input() public model!: {
-        id?: string;
-        permissions?: {
-            delete: boolean;
-        };
-    };
+    private canChange = true;
+    public isCreation = false;
+
+    public get model(): Model {
+        return this._model;
+    }
+
+    @Input()
+    public set model(value: Model) {
+        this._model = value;
+        if (this.canChange) {
+            this.isCreation = !this._model.id;
+        }
+    }
+
+    private _model!: Model;
 
     @Input() public form!: FormGroup;
 
     @Output() public readonly create = new EventEmitter<void>();
     @Output() public readonly delete = new EventEmitter<void>();
+
+    public constructor(route: ActivatedRoute) {
+        route.params.subscribe(() => (this.canChange = true));
+    }
 
     public clickCreate(): void {
         if (this.form.enabled) {
