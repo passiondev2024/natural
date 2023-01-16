d024334e72cb7bb4cb3daddcbb094639b156a4ca
kanandev2024 <kananramazandev@gmail.com>
2023-01-16 16:43:58 +0700

    As long as not part of public API can be exported for internal use

--- projects/natural/src/lib/modules/dropdown-components/type-text/type-text.component.ts
+++ projects/natural/src/lib/modules/dropdown-components/type-text/type-text.component.ts
@@ -7,7 +7,7 @@ import {NaturalDropdownRef} from '../../search/dropdown-container/dropdown-ref';
 import {NATURAL_DROPDOWN_DATA, NaturalDropdownData} from '../../search/dropdown-container/dropdown.service';
 import {DropdownComponent} from '../../search/types/dropdown-component';
 
-class InvalidWithValueStateMatcher implements ErrorStateMatcher {
+export class InvalidWithValueStateMatcher implements ErrorStateMatcher {
     public isErrorState(control: FormControl | null, form: FormGroupDirective | NgForm | null): boolean {
         return control && control.invalid && control.value;
     }
