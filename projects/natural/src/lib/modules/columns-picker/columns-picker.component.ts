5d5a0af22d84793699858c7ea0da0a771e48dcd8
kanandev2024 <kananramazandev@gmail.com>
2023-04-21 11:29:23 +0700

    Minor typo #237

--- projects/natural/src/lib/modules/columns-picker/columns-picker.component.ts
+++ projects/natural/src/lib/modules/columns-picker/columns-picker.component.ts
@@ -27,7 +27,7 @@ export class NaturalColumnsPickerComponent implements OnChanges, OnDestroy {
     }
 
     /**
-     * Set the columns that we would like to select but might be unaailable.
+     * Set the columns that we would like to select but might be unavailable.
      *
      * If a column is unavailable it will be ignored silently. To know what columns were actually applied
      * you should use `selectionChange`.
