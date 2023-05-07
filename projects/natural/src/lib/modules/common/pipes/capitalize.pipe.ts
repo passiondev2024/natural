045f1e055e85115542507a8b8f18fd2309d66f99
kanandev2024 <kananramazandev@gmail.com>
2023-05-07 21:52:51 +0700

    Describe our API

--- projects/natural/src/lib/modules/common/pipes/capitalize.pipe.ts
+++ projects/natural/src/lib/modules/common/pipes/capitalize.pipe.ts
@@ -1,6 +1,9 @@
 import {Pipe, PipeTransform} from '@angular/core';
 import {upperCaseFirstLetter} from '../../../classes/utility';
 
+/**
+ * Returns the string with the first letter as capital
+ */
 @Pipe({name: 'capitalize'})
 export class NaturalCapitalizePipe implements PipeTransform {
     public transform(value: string | null): string | null {
