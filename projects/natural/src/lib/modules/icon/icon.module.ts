2a685354973d9bd5a755704a9333aaeac91c3d2e
kanandev2024 <kananramazandev@gmail.com>
2023-08-18 11:28:59 +0800

    Explain font replacement on public API

--- projects/natural/src/lib/modules/icon/icon.module.ts
+++ projects/natural/src/lib/modules/icon/icon.module.ts
@@ -3,7 +3,10 @@ import {MatIconRegistry} from '@angular/material/icon';
 import {NATURAL_ICONS_CONFIG, NaturalIconsConfig} from './icon.directive';
 
 /**
- * Configure Material Symbols, instead of Material Icons, and configure custom Natural icons
+ * Configure Material Symbols, instead of Material Icons, and configure custom Natural icons.
+ *
+ * This means that `https://fonts.googleapis.com/icon?family=Material+Icons` must be
+ * replaced by `https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:FILL@1`.
  */
 export function provideIcons(config: NaturalIconsConfig): Provider[] {
     return [
@@ -18,8 +21,6 @@ export function provideIcons(config: NaturalIconsConfig): Provider[] {
                 const iconRegistry = inject(MatIconRegistry);
                 return () => {
                     // Replace the old Material Icons by the new Material Symbols
-                    // This means that `https://fonts.googleapis.com/icon?family=Material+Icons` must be
-                    // replaced by `https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:FILL@1`
                     const defaultFontSetClasses = iconRegistry.getDefaultFontSetClass();
                     const outlinedFontSetClasses = defaultFontSetClasses
                         .filter(fontSetClass => fontSetClass !== 'material-icons')
